0b2dd60295a1e8c03625ac45b0b569d8744d25ad
YoussefAMalek <youssef.ait.malek1@gmail.com>
2025-03-30 18:32:13 +0000

    Player movement script added

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 5;
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float Horizontal = Input.GetAxis("Horizontal") * speed;
+        float Vertical = Input.GetAxis("Vertical") * speed;
+
+        Horizontal *= Time.deltaTime;
+        Vertical *= Time.deltaTime;
+
+        transform.Translate(Horizontal, 0, Vertical);
+
+        if (Input.GetKeyDown("escape"))
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
     }
 }
