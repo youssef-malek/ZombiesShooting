6a1ecfa0924d7a0ea3e86a1db6aa96ca14bd957a
YoussefAMalek <youssef.ait.malek1@gmail.com>
2025-03-30 19:37:53 +0000

    First person perspective added

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5;
+    public float jumpForce = 4;
+
+    Rigidbody rb;
+    CapsuleCollider capsuleCollider;
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        rb = GetComponent<Rigidbody>();
+        capsuleCollider = GetComponent<CapsuleCollider>();
 
     }
 
@@ -22,9 +28,24 @@ public class PlayerMovement : MonoBehaviour
 
         transform.Translate(Horizontal, 0, Vertical);
 
+
+        if (isGrounded() && Input.GetButtonDown("Jump"))
+        {
+
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+
+        }
+
+
         if (Input.GetKeyDown("escape"))
         {
             Cursor.lockState = CursorLockMode.None;
         }
+    }
+
+    private bool isGrounded()
+    {
+
+        return Physics.Raycast(transform.position, Vector3.down, capsuleCollider.bounds.extents.y + 0.1f);
     }
 }
